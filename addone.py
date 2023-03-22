c7395006ca205d43e4f1f52b80915c44c1fd1485
Sai Wang <sai.ny@mac.com>
2023-03-21 21:58:11 -0400

    add(x,y)

--- addone.py
+++ addone.py
@@ -1,6 +1,9 @@
 def addone(x):
     return int(x) + 1
 
+def add(x,y): 
+    return int(x) + int(y)
+
 if __name__ == "__main__":
     val = input("enter a number:")
     print(f'{val} + 1 = {addone(val)}')
